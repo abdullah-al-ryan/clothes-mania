93365933910627f9d9459cf66bd10f1938bf154c
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-05-07 17:37:45 +0600

    family tree with same data added

---
+++ src/Components/Uncle/Uncle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Cousin from '../Cousin/Cousin';
+
+const Uncle = () => {
+    return (
+        <div>
+            <h4>Uncle</h4>
+            <section className='flex'>
+                <Cousin></Cousin>
+
+            </section>
+        </div>
+    );
+};
+
+export default Uncle;
