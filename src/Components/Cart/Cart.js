597b0df93bf64d8d13ef0dc06ab1f34f252a0773
abdullah-al-ryan <abdullah15-13088@diu.edu.bd>
2023-05-07 16:49:10 +0600

    cart added

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Cart = ({ cart, handleRemoveItem }) => {
+    return (
+        <div>
+            <h3>Order summary</h3>
+            <h5>Order Quantity: {cart.length}</h5>
+            {
+                cart.map(tshirt => <p
+                    key={tshirt._id}
+                >{tshirt.name}
+                    <button onClick={() => handleRemoveItem(tshirt)}>X</button></p>)
+            }
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/_steps.js
@@ -0,0 +1,10 @@
+/*
+1. npm i react-router-dom
+2. create a router using createBrowserRouter
+3. add RouterProvider and set router prop
+4. Create a lot of components
+5. Create main layout with some common part and one changing part based on the routes
+6. set main layout at the root of the route
+7. set children routes
+8. set links on the header components
+*/
